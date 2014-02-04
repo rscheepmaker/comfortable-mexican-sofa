3be2962ca47ca3164539a11dcb97e1d233bfad66
Rob Scheepmaker <rob@rscheepmaker.nl>
2014-02-04 11:52:25 +0100

    compatibility with locales

--- lib/comfortable_mexican_sofa/routes/cms_admin.rb
+++ lib/comfortable_mexican_sofa/routes/cms_admin.rb
@@ -4,33 +4,35 @@ class ActionDispatch::Routing::Mapper
     options[:path] ||= 'admin'
 
     scope :module => :admin do
-      namespace :cms, :as => :admin_cms, :path => options[:path], :except => :show do
-        get '/', :to => 'base#jump'
-        resources :sites do
-          resources :pages do
-            get  :form_blocks,    :on => :member
-            get  :toggle_branch,  :on => :member
-            put :reorder,         :on => :collection
-            resources :revisions, :only => [:index, :show, :revert] do
-              patch :revert, :on => :member
+      scope "(/:locale)", :locale => /nl|de|en/ do
+        namespace :cms, :as => :admin_cms, :path => options[:path], :except => :show do
+          get '/', :to => 'base#jump'
+          resources :sites do
+            resources :pages do
+              get  :form_blocks,    :on => :member
+              get  :toggle_branch,  :on => :member
+              put :reorder,         :on => :collection
+              resources :revisions, :only => [:index, :show, :revert] do
+                patch :revert, :on => :member
+              end
             end
-          end
-          resources :files do
-            post :reorder, :on => :collection
-          end
-          resources :layouts do
-            put :reorder, :on => :collection
-            resources :revisions, :only => [:index, :show, :revert] do
-              patch :revert, :on => :member
+            resources :files do
+              post :reorder, :on => :collection
             end
-          end
-          resources :snippets do
-            put :reorder, :on => :collection
-            resources :revisions, :only => [:index, :show, :revert] do
-              patch :revert, :on => :member
+            resources :layouts do
+              put :reorder, :on => :collection
+              resources :revisions, :only => [:index, :show, :revert] do
+                patch :revert, :on => :member
+              end
             end
+            resources :snippets do
+              put :reorder, :on => :collection
+              resources :revisions, :only => [:index, :show, :revert] do
+                patch :revert, :on => :member
+              end
+            end
+            resources :categories
           end
-          resources :categories
         end
       end
     end
